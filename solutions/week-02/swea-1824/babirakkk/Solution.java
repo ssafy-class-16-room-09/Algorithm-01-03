c966a4845e6a33c1ccf33f376ea985a65a9bec24
babirakkk <ybkang1108@gmail.com>
2026-08-16 12:38:22 +0900

    solve: SWEA 1824 혁진이의 프로그램 검증 주석 추가

--- solutions/week-02/swea-1824/babirakkk/Solution.java
+++ solutions/week-02/swea-1824/babirakkk/Solution.java
@@ -28,6 +28,14 @@ class Solution {
 		br.close();
 	}
 	
+
+	/**
+	 * 하나의 tc에 대해 혁진이의 프로그램 확인
+	 * BFS를 활용해 모든 가능한 실행 경로 탐색
+	 * 
+	 * @return 프로그램이 정지 문자(@)에 도달할 수 있으면 "YES", 도달할 수 없으면 "NO"
+	 * @throws IOException 입력 스트림 읽기 실패 시 발생
+	 */
 	private static String program() throws IOException {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		r = Integer.parseInt(st.nextToken());
@@ -69,6 +77,48 @@ class Solution {
 		return canStop ? "YES" : "NO";
 	}
 	
+
+	/**
+	 * 현재 명령어를 읽어 진행 방향과 메모리 상태를 업데이트하고 다음 위치 상태 배열을 반환
+	 * 
+	 * @param cmd 현재 위치의 수행 명령어
+	 * @param curr 현재 상태 배열 [r, c, dir, mem]
+	 * @return  다음 위치 상태 배열 [r, c, dir, mem], 만약 이미 방문한 상태라면 null 반환
+	 */
+	private static int[] processAndNext(char cmd, int[] curr) {
+		int currX = curr[0];
+		int currY = curr[1];
+		int dir = curr[2];
+		int mem = curr[3];
+				
+		switch (cmd) {
+			case '<' -> dir = LEFT;
+			case '>' -> dir = RIGHT;
+			case '^' -> dir = UP;
+			case 'v' -> dir = DOWN;
+			case '_' -> dir = (mem == 0) ? RIGHT : LEFT;
+			case '|' -> dir = (mem == 0) ? DOWN : UP;
+			case '+' -> mem = (mem == 15) ? 0 : mem + 1;
+			case '-' -> mem = (mem == 0) ? 15 : mem - 1;
+		}
+		
+		if (cmd >= '0' && cmd <= '9') { // 숫자라면 숫자를 메모리에 저장 
+			mem = cmd - '0';
+		}
+		
+		return move(currX, currY, dir, mem);
+	}
+
+
+	/**
+	 * 현재 위치와 상태를 기반으로 다음 위치 및 상태 배열을 계산해 반환
+	 * 
+	 * @param currX 현재 r 위치
+	 * @param currY 현재 c 위치
+	 * @param dir 진행 방향(0: UP, 1: DOWN, 2: LEFT, 3: RIGHT)
+	 * @param mem 현재 메모리 값
+	 * @return 다음 상태 배열 [r, c, dir, mem], 만약 이미 방문한 상태인 경우 null 반환
+	 */
 	private static int[] move(int currX, int currY, int dir, int mem) {
 		currX += dx[dir];
 		currY += dy[dir];
@@ -95,56 +145,13 @@ class Solution {
 		return new int[] {currX, currY, dir, mem};
 	}
 	
-	private static int[] processAndNext(char cmd, int[] curr) {
-		int currX = curr[0];
-		int currY = curr[1];
-		int dir = curr[2];
-		int mem = curr[3];
-				
-		switch (cmd) {
-		case '<':
-			dir = LEFT;
-			break;
-		case '>':
-			dir = RIGHT;
-			break;
-		case '^':
-			dir = UP;
-			break;
-		case 'v':
-			dir = DOWN;
-			break;
-		case '_':
-			if (mem == 0) {
-				dir = RIGHT;
-			} else {
-				dir = LEFT;
-			}
-			break;
-		case '|':
-			if (mem == 0) {
-				dir = DOWN;
-			} else {
-				dir = UP;
-			}
-			break;
-		case '.':
-			break;
-		case '+':
-			mem = (mem == 15) ? 0 : mem + 1;
-			break;
-		case '-':
-			mem = (mem == 0) ? 15 : mem - 1;
-			break;
-		}
-		
-		if (cmd >= '0' && cmd <= '9') { // 숫자라면 숫자를 메모리에 저장 
-			mem = cmd - '0';
-		}
-		
-		return move(currX, currY, dir, mem);
-	}
-	
+
+	/**
+	 * 명령어 데이터를 읽어 2차원 배열에 채움
+	 * 상하좌우 테두리를 OUT_OF_BOUND 문자로 채워 격자 경계 구분
+	 * 
+	 * @throws IOException 입력 스트림 읽기 실패 시 발생
+	 */
 	private static void fillMap()  throws IOException {
 		Arrays.fill(cmdMap[0], OUT_OF_BOUND);
 		for (int i = 1; i < r + 1; i++) {
